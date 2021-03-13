00aef50ac175d08a742a9381760374a023e03a1a
Soumya Tanwar <soumyatanwar23@gmail.com>
2021-03-13 18:57:07 +0530

    Added view functionality (separate from data model) for efficient testing:

--- services.py
+++ services.py
@@ -1,11 +1,10 @@
 from models import ToDoModel
-class ToDoService:
-    # Service class seperates views (what the user sees & gets from the data models)
-    # this enables to easily test the functions
 
+# Service class seperates views (what the user sees & gets from the data models)
+# this enables to easily test the functions
+class ToDoService:
     def __init__(self):
         self.model = ToDoModel()
 
     def create(self, params):
-        return self.model.create(params["Title", "Description"])
-        
+        return self.model.create(params["Title"], params["Description"])
