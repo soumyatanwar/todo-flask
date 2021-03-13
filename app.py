a3141e05257add812c4284d9dc9266550e47f4c7
Soumya Tanwar <soumyatanwar23@gmail.com>
2021-03-13 19:00:27 +0530

    Created route for users to interact with todos

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask # importing flask
 from models import Schema
+from services import ToDoService
 app = Flask(__name__) # Create an app instance
 
 @app.route("/") # at / endpoint do what - the what follows
@@ -9,6 +10,11 @@ def hello(): # call function hello
 @app.route("/<name>")
 def hello_with_name(name):
     return "Hello, "+name
+
+# Adding todo invocation route via POST request
+@app.route("/todo", methods=["POST"])
+def create_todo():
+    return ToDoService().create(request.get_json())
 
 if __name__=='__main__': # whenever app.py is run, the flask app would be run
     Schema()
